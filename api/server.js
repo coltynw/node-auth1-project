7bd4869bef1547c6833b688a048b9baf684b26de
coltynw <coltynw@users.github.com>
2020-01-29 22:59:38 -0500

    done

--- api/server.js
+++ api/server.js
@@ -1,6 +1,39 @@
 const express = require('express');
+
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+const dbConnection = require('../data/dbConfig');
+
+const usersRouter = require('../users/usersRouter');
+
+
 const server = express();
 
+const sessionConfig = {
+    name: 'monkey',
+        secret:'keep it secret, keep it safe!',
+        cookie: {
+            maxAge: 1000 * 30,
+            secure: false, 
+            httpOnly: true,
+        },
+        resave: false,
+        saveUninitialized: false, 
+        // don't forget 'new'
+        store: new KnexSessionStore({ 
+            knex: dbConnection,
+            tablename: 'sessions',
+            sidfieldname: 'sid',
+            createtable: true,
+            clearInterval: 60000
+        })
+}
+server.use(session(sessionConfig));
+
+
+
 server.use(express.json());
+server.use('/api', usersRouter)
+
 
 module.exports = server
--- users/usersRouter.js
+++ users/usersRouter.js
@@ -23,6 +23,8 @@ router.post('/login', (req, res) => {
      userThing.findBy({username})
     .then(user => {
         if(user && bcrypt.compareSync(password, user.password)){
+            req.session.loggedIn = true;
+            req.session.userId = user.id;
             res.status(200).json({message: `Welcome ${user.username}`})
         } else {
             res.status(401).json({message: 'error'});
